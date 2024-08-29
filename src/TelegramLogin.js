e074ea884be184999dbee0ca55e32c37d024c377
new <new@Pranavs-Mac-mini.local>
2024-08-30 02:48:16 +0530

    commit

--- src/TelegramLogin.js
+++ src/TelegramLogin.js
@@ -7,23 +7,33 @@ const TelegramLogin = () => {
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
     script.setAttribute('data-telegram-login', 'ConnectVerseBot');
-    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-size', 'medium');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     script.setAttribute('data-request-access', 'write');
     document.getElementById('telegram-login').appendChild(script);
   }, []);
 
   const onTelegramAuth = (user) => {
+    
     alert(`Logged in as ${user.first_name} ${user.last_name} (${user.id}${user.username ? ', @' + user.username : ''})`);
+    
+    // Change the button text after successful authentication
+    const buttonElement = document.querySelector('.telegram-login-button'); // Update this selector based on actual button class or ID
+    if (buttonElement) {
+      buttonElement.innerText = 'Logged in as @'+user.username;
+    }
+    
+   
   };
 
-  // Make sure this function is globally accessible
   window.onTelegramAuth = onTelegramAuth;
 
   return (
-    <div id="telegram-login">
-      {/* Telegram login button will be injected here */}
-    </div>
+ 
+      <div id="telegram-login">
+       
+      </div>
+  
   );
 };
 
