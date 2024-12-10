67f24da4ab2a6dbbb121e28ea33b92456d2052d0
Dev <Dev@Pranavs-MacBook-Air.local>
2024-12-10 23:47:15 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './images/logo.avif'
 import homeimg from './images/homeimg.png'
 import bgvideo from './images/bgvideo.mp4'
+import elevate from './images/Elevate.mp4'
 import './Home.css'
 import Carousel from 'react-bootstrap/Carousel';
 import irl1 from './images/irl1.avif'
@@ -30,6 +31,24 @@ import partnersimage2 from './images/partnersimage2.png'
 import { XEmbed } from 'react-social-media-embed';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { SocialIcon } from 'react-social-icons'
+import Typewriter from "typewriter-effect";
+import moi from './images/moi.png'
+import plutope from './images/plutope.avif'
+import polygon from './images/polygon.png'
+import router from './images/router.png'
+import qoneqt from './images/qoneqt.png'
+import icp from './images/icp.png'
+import stellar from './images/stellar.png'
+import bharatbox from './images/bharatbox.png'
+import inco from './images/inco.avif'
+import coinstore from './images/coinstore.avif'
+import graph from './images/graph.png'
+import propeers from './images/propeers.avif'
+import defiverse from './images/defiverse.avif'
+import ai from './images/ai.gif'
+import homeimage from './images/homeimage.gif'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function Home() {
@@ -43,21 +62,24 @@ function Home() {
   
     const projectId = 'd1b8ecd12b45222ded58ecacaf0afcce'
 
+    const notify = () => toast("Email copid to clipboard !");
+    const notify1 = () => toast("Submitted. We will reach out to you soon !");
+
 
 
   return (
     <div>
   
 
-      <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary" fixed='top'>
+      <Navbar expand="lg" data-bs-theme="dark" className="bg-black" type="sticky">
       <Container>
       <Navbar.Brand href="#home"><img style={{width:'10em'}} src={logo}></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#events">Events</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
 
             <Nav.Link>
 
@@ -101,193 +123,297 @@ function Home() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-      <div className="video-container">
-        <div id="home"></div>
-      <video autoPlay loop muted playsInline className="background-video">
-        <source src={bgvideo} type="video/mp4" />
+ <br></br>
+ <div class="homepage">
+ 
+    {/* <video
+      class="homeimg"
+        autoPlay
+        loop
+        muted
+        src={elevate} // Replace with your video path
+     
+      >
         Your browser does not support the video tag.
-      </video>
+      </video> */}
+      <img src={homeimage} class="homeimg"></img>
+  
+   
+    
+      <a href='https://calendly.com/quantumworld394/30min'><button class="button-85" role="button">Book a Call</button></a>
+     
+<br></br>
+     
+    <br></br>
+
+    
       {/* Other content can go here */}
-      <div className="content">
-        <br></br>  <br></br>  <br></br>
-      <img className="homeimg" src={homeimg}/>
-  <br/>
-
-
-<a href='https://t.me/connectverseweb3'><button class="button-85" role="button">Join Community</button></a>
-<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-
-
-  {/* Carousal */}
-
+      
+  
+  
+      
+   
+          
+            
+   
+    
   
 
-      </div>
-    </div>
+    
+ 
+   
+   
+  
+  
 
-    <div className="video-container" style={{backgroundColor:'black'}}>
+
+  <section style={{color:'white'}}>
+        <h2>Trusted By</h2>
+        We are proud to collaborate with our esteemed partners.
+        <br></br> <br></br> <br></br> 
+        <div class="slider">
+            <div class="slider-items">
+                <img src={icp}
+                    alt="" />
+                <img style={{width:'15em'}} src={router}
+                    alt="" />
+                <img src={bharatbox}
+                    alt="" />
+                <img src={qoneqt}
+                    alt="" />
+                <img style={{width:'5em'}} src={graph}
+                    alt="" />
+                <img style={{width:'15em'}} src={coinstore} alt=""/>
+                <img src={polygon}
+                    alt="" />
+                     <img style={{width:'5em'}} src={moi}
+                    alt="" />
+                <img style={{width:'15em'}} src={stellar}
+                    alt="" />
+              
+               
+                <img style={{width:'10em'}} src={plutope}
+                    alt="" />
+                <img src={polygon}
+                    alt="" />
+                <img src={defiverse}
+                    alt="" />
+                <img src={propeers} alt=""/>
+               
+
+
+            </div>
+        </div>
+
+    </section>
+
+    <br></br>
+    <br></br>
+<div style={{color:'white'}}>
+  <h2>
+    <Typewriter 
+                onInit={(typewriter) => {
+                    typewriter
+                        .typeString("250+ IRL Events Conducted")
+                        .pauseFor(200)
+                        .deleteAll()
+                        .typeString("20,000+ Developers Onboarded")
+                        .pauseFor(200)
+                        .deleteAll()
+                        .typeString("50+ Happy Cleints")
+                        .start();
+                }}
+                options={{
+                  wrapperClassName: 'text-white'  // Makes the typewriter text white
+              }}
+
+              
+
+              
+            />
+            </h2>
+            </div>
+<br></br>
+<Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+      <img style={{width:'90%'}} src={irl1}></img>
+        <Carousel.Caption>
+        
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img style={{width:'90%'}}src={irl2}></img>
+        <Carousel.Caption>
       
-      {/* Other content can go here */}
-      <div className="content">
-      <ScrollAnimation animateIn='flipInY'
-  animateOut='flipOutY'>
-      <h1> Some of our Past Events</h1>
-      </ScrollAnimation>
-      <br></br>
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-      <img src={irl1}></img>
-        <Carousel.Caption>
-         6+ IRLs with Graph Protocol
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={irl2}></img>
+      <img style={{width:'90%'}} src={irl3}></img>
         <Carousel.Caption>
-         20+ IRls with Router Protocol
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={irl3}></img>
-        <Carousel.Caption>
-        20+ IRls with Inco Protocol
+       
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-      <img src={irl4}></img>
+      <img style={{width:'90%'}} src={irl4}></img>
         <Carousel.Caption>
-       Web3Utsav Roadshow
+     
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={irl5}></img>
-        <Carousel.Caption>
-      Web3 Community Call
+      <img style={{width:'90%'}} src={irl5}></img>
+      <Carousel.Caption>
+    
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={irl6}></img>
+      <img style={{width:'90%'}} src={irl6}></img>
         <Carousel.Caption>
-        IRL + Meetup
+      
         </Carousel.Caption>
       </Carousel.Item>
       
     </Carousel>
 
-    
- 
-      </div>
-    </div>
-    <div  style={{backgroundColor:'black'}}>
-
-   <br></br><br></br><br></br><br></br>
-   </div>
-    <div  style={{backgroundColor:'black'}}>
-      
-      {/* Other content can go here */}
-      <div className="content">
-  
-  <ScrollAnimation  animateIn='flipInY'
-  animateOut='flipOutY'>  <h1>Trusted By</h1>
-  We are proud to collaborate with our esteemed partners.</ScrollAnimation>
-
-  <br></br>
-  
-  <br></br>
- <Marquee>
-<img className="partnerimg" style={{width:'70em'}} src={partnersimage}></img>
-<img className="partnerimg" style={{width:'70em'}} src={partnersimage2}></img>
-</Marquee>
  <br></br><br></br>
- <div id='contact'></div>
-<a href='https://calendly.com/quantumworld394/30min'><button class="button-85" role="button">Book a Call</button></a>
-<br></br><br></br>
-      </div>
-    </div>
+ <h2 style={{color:'white'}}> Automate your onboarding process with ConnectVerse</h2>
+<img style={{width:'20em'}} src={ai}></img>
+<br></br>
+<div id="pricing"></div>
+<h2 style={{color:'white'}}> Pricing</h2>
+<br></br>
+  
+  <div class="pricing" style={{color:'white'}}>
+<div class="glass" style={{width:'20em',height:'28em', border: "1px solid white"}}>
 
-    <div  style={{backgroundColor:'black'}}>
-      
-      {/* Other content can go here */}
-      <div className="content">
+<div class="pricing-header" style={{width:'100%',height:'5em',backgroundColor:'#8c52ff'}}>
+<h3>Basic</h3>
+</div>
+<div class="pricing-content" style={{height:'20em'}}>
+<div class="pricing-body">
+
+
+<br/>
+<h3>$3800</h3>
+<br/>
+  <h5>Workshop duration of 50 mins </h5>
+  <br/>
+  <h5>IRL Workshop in 25 Colleges </h5>
+  <br/>
+  <h5>Interact with over 2000 students </h5>
   
 
 
-  <br></br>
-  <br></br>
+
+</div>
+</div>
+<a href='https://calendly.com/quantumworld394/30min'><button class="button-85" role="button">Book a Call</button></a>
+</div>
+
+&nbsp;&nbsp;
+<div class="glass" style={{width:'20em',height:'28em', border: "1px solid white"}}>
+
+<div class="pricing-header" style={{width:'100%',height:'5em',backgroundColor:'#8c52ff'}}>
+<h3>Basic</h3>
+</div>
+<div class="pricing-content" style={{height:'20em'}}>
+<div class="pricing-body">
+
+
+<br/>
+<h3>$3800</h3>
+<br/>
+  <h5>Workshop duration of 50 mins </h5>
+  <br/>
+  <h5>IRL Workshop in 25 Colleges </h5>
+  <br/>
+  <h5>Interact with over 2000 students </h5>
+  
 
 
 
+</div>
+</div>
+<a href='https://calendly.com/quantumworld394/30min'><button class="button-85" role="button">Book a Call</button></a>
+</div>
+&nbsp;&nbsp;
+<div class="glass" style={{width:'20em',height:'28em', border: "1px solid white"}}>
+
+<div class="pricing-header" style={{width:'100%',height:'5em',backgroundColor:'#8c52ff'}}>
+<h3>Basic</h3>
+</div>
+<div class="pricing-content" style={{height:'20em'}}>
+<div class="pricing-body">
 
 
-      </div>
-    </div>
- 
-    <div class="tweets"  style={{backgroundColor:'black'}}>
-    <div style={{backgroundColor:'black',color:'white',paddingRight:'2em'}}>
-    <ScrollAnimation  animateIn='flipInY'
-  animateOut='flipOutY'>  <h1>Testimonials</h1>
-  Check out some of the buzz on Twitter about the events we've hosted! 🥳
- </ScrollAnimation>
+<br/>
+<h3>$3800</h3>
+<br/>
+  <h5>Workshop duration of 50 mins </h5>
+  <br/>
+  <h5>IRL Workshop in 25 Colleges </h5>
+  <br/>
+  <h5>Interact with over 2000 students </h5>
+  
+
+
+
+</div>
+</div>
+<a href='https://calendly.com/quantumworld394/30min'><button class="button-85" role="button">Book a Call</button></a>
+</div>
+</div>
  </div>
-<br></br>
-<ScrollAnimation duration={3}
- animateIn='flipInY'
- animateOut='flipOutY'>
-      <div style={{ display: 'flex', justifyContent: 'center' ,paddingRight:'2em'}}>
-  <XEmbed url="https://x.com/ConnectVerse3/status/1772979373919932796" width={325} />
-</div>
-    </ScrollAnimation>
-    <ScrollAnimation duration={3}
- animateIn='flipInY'
- animateOut='flipOutY'>
-<div style={{ display: 'flex', justifyContent: 'center' ,paddingRight:'2em'}}>
-  <XEmbed url="https://x.com/SuperRabbitsDAO/status/1767123410407342332" width={325} />
-</div>
-</ScrollAnimation>
-<ScrollAnimation duration={3}
- animateIn='flipInY'
- animateOut='flipOutY'>
-<div style={{ display: 'flex', justifyContent: 'center' ,paddingRight:'2em'}}>
-  <XEmbed url="https://x.com/qoneqtapp/status/1762155926541504607" width={325} />
-</div>
-</ScrollAnimation>
-<ScrollAnimation duration={3}
- animateIn='flipInY'
- animateOut='flipOutY'>
-<div style={{ display: 'flex', justifyContent: 'center' ,paddingRight:'2em'}}>
-  <XEmbed url="https://x.com/TheGraphIndia/status/1719741164134482123" width={325} />
-</div>
-</ScrollAnimation>
-<ScrollAnimation duration={3}
- animateIn='flipInY'
- animateOut='flipOutY'>
-<div style={{ display: 'flex', justifyContent: 'center' ,paddingRight:'2em'}}>
-  <XEmbed url="https://x.com/TheGraphIndia/status/1718459249939152934" width={325} />
-</div>
-</ScrollAnimation>
-
-</div>
+ 
+    
 
    
     <center>
-   
+
+      <br/><br></br>
+      <div id="contact"></div>
+ 
 <div style={{backgroundColor:'black'}}> 
-<br></br> <br></br> <br></br> <br></br>
-    <iframe
-src="https://www.chatbase.co/chatbot-iframe/dKPjWH2ngM-yLWxO0iOb4"
-width="100%"
-style={{height:'100%',minHeight:'700px'}}
-frameborder="0"
-></iframe>
+<h2 style={{color:'white'}}>Contact</h2>  
 <br></br>
-<br></br><br></br><br></br><br></br>
+<center>
+
+  <br/>
+  <input style={{width:'40%',height:'3em'}} placeholder="Enter Name"></input>
+  <br/>
+  <br/>
+  <input style={{width:'40%',height:'3em'}} placeholder="Enter Company Name"></input>
+  <br/>
+  <br/>
+  <textArea style={{width:'40%',height:'10em'}} placeholder="Description"></textArea>
+  <br/>
+  <br></br>
+  <button style={{width:'40%',height:'3em'}} class="button-85" onClick={()=>{
+    notify1()
+  }}>Submit</button>
+</center>
+<br></br>
 
 <br></br><br></br><br></br><br></br>
+<hr style={{backgroundColor:'white', color:'white'}}></hr>
+<br></br><br></br><br></br><br></br>
+<ToastContainer />
 <div style={{color:'white'}}>
 Find Us On
 <br></br><br></br><br></br>
+<SocialIcon network="email" onClick={()=>{
+  navigator.clipboard.writeText("connectversesp@gmail.com").then(
+    () => {
+     notify()
+    },
+    (err) => {
+      alert("Failed to copy email address. Please try again.");
+      console.error("Error copying email to clipboard: ", err);
+    }
+  );
+}}/>
+
+&nbsp;&nbsp;&nbsp;
 <SocialIcon url="https://x.com/connectverse3" />
 &nbsp;&nbsp;&nbsp;
 <SocialIcon url="https://www.youtube.com/channel/UCXFScMQoAZCphx44xk0u27Q"/>
@@ -314,3 +440,6 @@ All Rights Reserved
 }
 
 export default Home
+
+
+
